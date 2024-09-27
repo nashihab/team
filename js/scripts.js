646941d17ef926dac6dd529dd9fca2009637995d
Nahid Ahmed <nahidahmedshihab@gmail.com>
2024-09-27 17:15:00 +0600

    Update 2.0

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
-    particlesJS.load('particles-js', 'particles.json', function() {
-        console.log('callback - particles.js config loaded');
-    });
+
 
     // Preloader removal
     window.onload = function() {
         const preloader = document.getElementById('preloader');
         preloader.style.display = 'none';
     };
-});
+
